88a33950a7179ec9729432b2cba8817758322c5d
Sean <Seanr1194@gmail.com>
2019-10-02 09:25:08 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/coloractivities/CanvasActivity.java
@@ -0,0 +1,6 @@
+package edu.temple.coloractivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CanvasActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/edu/temple/coloractivities/PaletteActivity.java
@@ -0,0 +1,56 @@
+package edu.temple.coloractivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class PaletteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] colors = {"Select a color", "Red", "Yellow", "Green", "Teal", "Cyan", "Blue",
+                "Magenta", "Purple", "Maroon", "Black", "Gray"};
+
+        // Create Adapter
+        ColorAdapter colorAdapter = new ColorAdapter(PaletteActivity.this, colors);
+
+        // Obtain spinner
+        Spinner colorSpinner = findViewById(R.id.spinner);
+        colorSpinner.setAdapter(colorAdapter);
+
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                //Change textview to white
+                view.setBackgroundColor(Color.WHITE);
+                ((TextView) view).setTextColor(Color.BLACK);
+
+                //set background to color
+                ConstraintLayout layout = findViewById(R.id.layout);
+
+                String colorValue = ((TextView) view).getText().toString();
+                if (colorValue.equals("Select a color")) colorValue = "White";
+
+                layout.setBackgroundColor(Color.parseColor(colorValue));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+    }
+
+
+}
